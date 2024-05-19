5585a0da73195dffa42571645e93987723881af7
TranQuocBaoGit <tranquocbaodgnl@gmail.com>
2024-05-19 13:41:25 +0700

    fix env configs

--- configs/env_config.go
+++ configs/env_config.go
@@ -1,6 +1,8 @@
 package configs
 
-import "github.com/spf13/viper"
+import (
+	"os"
+)
 
 type Config struct{
 	ALCHEMY_API_KEY		string `mapstructure:"ALCHEMY_API_KEY"`
@@ -12,17 +14,30 @@ type Config struct{
 }
 
 func LoadConfig(path string) (config Config, err error){
-	viper.AddConfigPath(path)
-	viper.SetConfigName(".env")
-	viper.SetConfigType("env")
+	// viper.AddConfigPath(path)
+	// viper.SetConfigName(".env")
+	// viper.SetConfigType("env")
 
-	viper.AutomaticEnv()
+	// viper.AutomaticEnv()
 
-	err = viper.ReadInConfig()
-	if err != nil{
-		return
-	}
+	// err = viper.ReadInConfig()
+	// if err != nil{
+	// 	return
+	// }
 
-	err = viper.Unmarshal(&config)
-	return
+	alchemy := os.Getenv("ALCHEMY_API_KEY")
+	etherscan := os.Getenv("ETHER_SCAN_API")
+	server := os.Getenv("SERVER")
+	mongo := os.Getenv("MONGO_URI")
+	database := os.Getenv("DATABASE_NAME")
+
+	// err = viper.Unmarshal(&config)
+	return Config{
+		ALCHEMY_API_KEY: alchemy,
+		ETHER_SCAN_API: etherscan,
+		SERVER: server,
+		CONTRACT_PATH: "",
+		MONGO_URI: mongo,
+		DATABASE_NAME: database,
+	}, nil
 }
